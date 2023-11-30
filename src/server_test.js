7f5674108f4bb7a2da2ee87d2b4ef34ed17c7352
NhatLink <116991698+NhatLink@users.noreply.github.com>
2023-11-30 23:55:18 +0700

    setup file .env

--- src/server_test.js
+++ src/server_test.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 //config template engine
 app.set("views", path.join(__dirname, "views"));
@@ -17,6 +20,6 @@ app.get("/link", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
